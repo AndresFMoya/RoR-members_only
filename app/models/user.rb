9e8cd9e4b805a3c43bb75513d4b1ac2d38464088
KerronKing <king_of_palmiste@hotmail.com>
2019-11-04 12:42:12 -0400

    Add digest generation methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update(:remember_digest, User.digest(remember_token))
+  end
 end
