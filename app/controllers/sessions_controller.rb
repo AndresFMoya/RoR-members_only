aca08548f292adf04086283c696a45bc367e4b39
AndresFMoya <andresfmoya@gmail.com>
2019-11-04 15:29:54 -0500

    Add create method to sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    user && user.authenticate(params[:session][:password])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,21 @@ require "digest"
 
 class User < ApplicationRecord
   attr_accessor :remember_token
-  before_create :remember
   has_secure_password
 
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  # Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
-  def User.digest(string)
-    Digest::SHA1.hexdigest string
-  end
-
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
-    update(remember_digest: User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
