321e0c51c70baa1be9a58a0c5338036eaeb5df16
KerronKing <king_of_palmiste@hotmail.com>
2019-11-04 17:10:43 -0400

    Add current_user method to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   # Logs in the given user.
   def log_in(user)
-  session[:user_id] = user.id
+    session[:user_id] = user.id
   end
 
   # Remembers a user in a persistent session.
@@ -10,5 +10,18 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Retreives the current user
+  def current_user
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by(id: session[user_id])
+    elsif user_id = cookies.signed(:user_id)
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 end
