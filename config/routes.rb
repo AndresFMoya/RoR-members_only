549362d049e3df85962bddae460e0daba97ae8fb
KerronKing <king_of_palmiste@hotmail.com>
2019-11-04 17:29:11 -0400

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
