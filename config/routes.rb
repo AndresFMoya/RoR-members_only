cfb39070fbbf48d390204fc27d146c08c5f63ffe
KerronKing <king_of_palmiste@hotmail.com>
2019-11-04 12:30:56 -0400

    Update routes, initialize signup form and add remember token column to user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_154902) do
+ActiveRecord::Schema.define(version: 2019_11_04_162533) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_154902) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_token"
   end
 
 end
