761066fed33c60dd15c33dc8dcf265a26d5af508
AndresFMoya <andresfmoya@gmail.com>
2019-11-05 10:47:14 -0500

    Generate controller and model Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
