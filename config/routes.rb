5ec5c12784585e201b40422aa6a7f947cd2b36b1
KerronKing <king_of_palmiste@hotmail.com>
2019-11-07 12:30:09 -0400

    Add create action to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "/home", to: 'static_pages/home'
+  root 'static_pages#home'
   get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
