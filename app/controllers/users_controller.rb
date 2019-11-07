b2b4847df42ed36e9e8d64c9f5155c35e3c237ce
KerronKing <king_of_palmiste@hotmail.com>
2019-11-07 12:09:46 -0400

    Add application header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-<<<<<<< HEAD
   def show
     @user = User.find(params[:id])
   end
-=======
-  def show; end
->>>>>>> 4467325652115d2d5f222987f546b33a3b122b38
 end
