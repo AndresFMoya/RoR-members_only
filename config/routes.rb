ae9f656aaae4815a4050063c3c0f1fc25ea61d08
KerronKing <king_of_palmiste@hotmail.com>
2019-11-07 12:20:22 -0400

    Add home page with link to signup and edit routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'static_pages/home'
+  root "/home", to: 'static_pages/home'
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
-  root 'sessions#new'
   resources :posts
 end
