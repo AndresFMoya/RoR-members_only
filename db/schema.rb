e6161acb5ba69d6eba1d5e8817e35729f783da9d
AndresFMoya <andresfmoya@gmail.com>
2019-11-07 16:06:31 -0500

    Delete migration add_reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_210013) do
+ActiveRecord::Schema.define(version: 2019_11_05_160317) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
